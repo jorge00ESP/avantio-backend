23dd21f45382ea28d9e0f26d304c208c3a30e781
jorge00ESP <jorgerocamorabas@gmail.com>
2025-10-09 21:25:06 +0200

    change classes name

--- src/core/application/article/getElPaisArticle.use-case.ts
+++ src/core/application/article/getElPaisArticle.use-case.ts
@@ -2,7 +2,7 @@ import { DeleteResult } from "mongoose";
 import { ArticleRepository } from "../../ports/Article.repository";
 import { Article } from "../../domain/Article.entity";
 
-export class FindByUrlArticleUseCase{
+export class GetElPaisArticlesUseCase{
 
   constructor(private articleRepo: ArticleRepository){}
 
